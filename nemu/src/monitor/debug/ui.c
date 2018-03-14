512585214a49d7fc50d1d89ba466bc81a02b29b8
tracer-ics2017 <tracer@njuics.org>
2018-03-14 11:56:59 +0000

    >  compile 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  11:56:59 up  2:35,  5 users,  load average: 0.02, 0.02, 0.00 d86271bb53773a03c23f3f03ff96568887f79e09

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -98,11 +98,16 @@ static int cmd_x(char *args){
 	int N = atoi(arg);
 	arg = strtok(NULL, " ");
   uint32_t addr;
-	sscanf(arg, "0x%x", &addr);
+	if (!sscanf(arg, "0x%x", &addr))
+	{
+		printf("Please check arguments\n");
+		return -1;
+	}
+
 	int i;
 	for (i=1;i<=N;i++)
 	{
-		printf("0x%x		%x\n", addr, vaddr_read(addr, 32));
+		printf("0x%x		0x%x\n", addr, vaddr_read(addr, 32));
 		addr += 4;
 	}
 //	printf("%d %s\n",N,arg);

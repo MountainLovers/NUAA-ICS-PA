c02dc48340f3d9ad74cbe242def2df36476d78e0
tracer-ics2017 <tracer@njuics.org>
2018-03-21 11:13:57 +0000

    >  compile 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  11:13:57 up 39 min,  4 users,  load average: 0.02, 0.03, 0.00 15ac76ce5a449a0b6b6de7cd23778c2f82543f21

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -219,7 +219,7 @@ uint32_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-	printf("nr_token=%d\n",nr_token);	
+//	printf("nr_token=%d\n",nr_token);	
 	printf("%d\n",eval(0, nr_token-1));
   /* TODO: Insert codes to evaluate the expression. */
 /*  for (i = 0; i < nr_token; i++){

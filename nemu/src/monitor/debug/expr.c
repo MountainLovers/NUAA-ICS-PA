2e3d80f491630d8f3d6be220eff22f867ebb30f9
tracer-ics2017 <tracer@njuics.org>
2018-03-21 16:45:16 +0000

    >  compile 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  16:45:16 up  2:52,  4 users,  load average: 0.01, 0.02, 0.00 970e958f3fe2b39d13375cdbe942ea1c7fdf8d87

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -7,7 +7,7 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ, TK_DEC, TK_HEX, TK_REGU, TK_REG, TK_VAR, TK_MIMI, TK_PLPL, TK_UEQ, TK_AND, TK_OR, TK_NOT, TK_TANHAO
+  TK_NOTYPE = 256, TK_EQ, TK_DEC, TK_HEX, TK_REGU, TK_REG, TK_VAR, TK_MIMI, TK_PLPL, TK_UEQ, TK_AND, TK_OR, TK_NOT, TK_TANHAO, TK_JYY, TK_FS
 
   /* TODO: Add more token types */
 
@@ -148,6 +148,24 @@ bool check_parentheses(int p, int q) {
 	return true;
 }
 
+uint32_t value(int p, int q);
+
+uint32_t eval(int p, int q) {
+	if (p > q) {
+		printf("Bad expression!\n");
+		assert(0);
+	}
+	else {
+		switch (tokens[p].type) {
+			case TK_JYY: {int v=value(p+1, q); return vaddr_read(v, 4);}
+			case TK_FS: {int v=value(p+1, q); return -v;}
+			case TK_MIMI: {int v=value(p+1, q); return --v;}
+			case TK_PLPL: {int v=value(p+1, q); return ++v;}	
+			default: assert(0);	
+		}
+	}	
+}
+
 // normal
 uint32_t value(int p, int q) {
 	if (p > q) {
@@ -159,6 +177,33 @@ uint32_t value(int p, int q) {
 		uint32_t v;
 		if (tokens[p].type == TK_DEC) {sscanf(tokens[p].str, "%d", &v); return v;}
 		if (tokens[p].type == TK_HEX) {sscanf(tokens[p].str, "%x", &v); return v;}
+	  if (tokens[p].type == TK_REG) {	
+		  const char *reg32[] = {"eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi"};
+		  const char *reg16[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
+	 		const char *reg8[] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};	 
+			int l=strlen(tokens[p].str)-1;
+			char str[32];
+			strncpy(str,tokens[p].str+1,l);
+			int i,j=0;
+			for (i=0;i<l;i++) if (str[i] >= 'A' && str[i] <= 'Z') str[i]+=32;
+			if (l == 3 && !j) {
+				for (i=0;i<8;i++) 
+					if (strcmp(str,reg32[i]) == 0) {j=32; break;}
+			}
+			if (l == 2 && !j) {
+				for (i=0;i<8;i++) 
+					if (strcmp(str,reg16[i]) == 0) {j=16; break;}
+			}
+			if (l == 2 && !j) {
+				for (i=0;i<8;i++)
+					if (strcmp(str,reg8[i]) == 0) {j=8; break;}
+			}
+			if (j == 32) return reg_l(i);
+			if (j == 16) return reg_w(i);
+			if (j == 8) return reg_b(i);
+			assert(0);
+		}
+				
 		assert(0);
 	}
 	else if (check_parentheses(p, q) == true) {
@@ -178,6 +223,7 @@ uint32_t value(int p, int q) {
 //		opt_level[TK_UEQ] = 90;
 		opt_level[TK_AND] = 80;
 		opt_level[TK_OR] = 70;
+		opt_level[TK_JYY] = 500;
 		int pp;
 		int lowest_level = 1000, lowest_pos = -1, parenthese_flag = 0;
 		for (pp=p;pp<=q;pp++) {
@@ -188,6 +234,10 @@ uint32_t value(int p, int q) {
 							lowest_level = opt_level[tokens[pp].type];
 							lowest_pos = pp;
 			}
+		}
+
+		if (tokens[lowest_pos].type == TK_JYY) {
+			return eval(p, q);
 		}
 
 		uint32_t val1 = value(p, lowest_pos-1);
@@ -219,6 +269,10 @@ uint32_t expr(char *e, bool *success) {
   }
 	printf("%d\n",value(0, nr_token-1));
   /* TODO: Insert codes to evaluate the expression. */
-
+	int i;
+	for (i = 0; i < nr_token; i ++) {
+		if (tokens[i].type == '*' && (i == 0 || tokens[i-1].type == '+' || tokens[i-1].type == '-' || tokens[i-1].type == '*' || tokens[i-1].type == '/' || tokens[i-1].type == '%' || tokens[i-1].type == TK_MIMI || tokens[i-1].type == TK_PLPL || tokens[i-1].type == TK_EQ || tokens[i-1].type == TK_UEQ || tokens[i-1].type == TK_AND || tokens[i-1].type == TK_OR || tokens[i-1].type == TK_NOT || tokens[i-1].type == '('))
+			tokens[i].type = TK_JYY;	
+	}
   return 0;
 }

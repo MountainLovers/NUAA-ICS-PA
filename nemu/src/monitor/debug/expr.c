a9b51dd626e16f058b03d21a7caff1a932afad01
tracer-ics2017 <tracer@njuics.org>
2018-03-26 14:03:32 +0000

    >  compile 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  14:03:32 up 55 min,  3 users,  load average: 0.00, 0.01, 0.00 89f99b95c2e0761ebad77024eb4c608c2c700542

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -117,8 +117,8 @@ static bool make_token(char *e) {
         int substr_len = pmatch.rm_eo;
 
 				/*print Log information.*/
-//        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s----%d",
-//            i, rules[i].regex, position, substr_len, substr_len, substr_start,rules[i].token_type);
+        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s----%d",
+            i, rules[i].regex, position, substr_len, substr_len, substr_start,rules[i].token_type);
         position += substr_len;
 			
         /* TODO: Now a new token is recognized with rules[i]. Add codes

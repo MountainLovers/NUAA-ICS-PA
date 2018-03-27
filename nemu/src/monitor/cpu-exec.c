7591abd75670819f12f40cf1753e2b27129795b8
tracer-ics2017 <tracer@njuics.org>
2018-03-27 15:08:41 +0000

    >  compile 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  15:08:41 up 19 min,  4 users,  load average: 0.01, 0.01, 0.00 bfeb307bfcf8107a12618e22024bd829f0b4b94

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -31,14 +31,13 @@ void cpu_exec(uint64_t n) {
 
 #ifdef DEBUG
     /* TODO: check watchpoints here. */
-
+		if (check_wp() == true) nemu_state = NEMU_STOP;
 #endif
 
 #ifdef HAS_IOE
     extern void device_update();
     device_update();
 #endif
-		if (check_wp() == true) nemu_state = NEMU_STOP;
 
     if (nemu_state != NEMU_RUNNING) { return; }
   }

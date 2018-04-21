a7ee33d32983c755ed8ee96149c9c537189b7c31
tracer-ics2017 <tracer@njuics.org>
2018-04-21 17:18:58 +0000

    >  compile 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  17:18:58 up  2:45,  6 users,  load average: 0.00, 0.00, 0.00 78a0a416de4b7ad6e8352b5ddff6fab63fcffd37

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -29,7 +29,6 @@ make_EHelper(call) {
 	rtl_push(&decoding.seq_eip);
 	decoding.is_jmp = 1;
 //	decoding.jmp_eip = decoding.seq_eip + id_dest->val;
-//	print_asm("cpu.eip = %x ; decoding.seq_eip = %x ;id_dest->val = %d", cpu.eip, decoding.seq_eip, id_dest->val);
   print_asm("call %x", decoding.jmp_eip);
 }
 

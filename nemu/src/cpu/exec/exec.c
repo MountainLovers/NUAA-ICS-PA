3fe5691a1e380b126326a3ccd52127d1999f1551
tracer-ics2017 <tracer@njuics.org>
2018-04-25 10:32:22 +0000

    >  compile 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  10:32:22 up 30 min,  6 users,  load average: 0.00, 0.00, 0.00 b4e69986bdc1038effe03139876e9117e1104d3a

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -61,8 +61,8 @@ make_group(gp4,
 
   /* 0xff */
 make_group(gp5,
-    IDEX(E, inc), IDEX(E, dec), IDEX(E, call), EMPTY,
-    IDEX(E, jmp), EMPTY, EX(push), EMPTY)
+    EMPTY, EMPTY, EMPTY, EMPTY,
+    EMPTY, EMPTY, EX(push), EMPTY)
 
   /* 0x0f 0x01*/
 make_group(gp7,

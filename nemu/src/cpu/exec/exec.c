ccd06fa1bf81b741f3ba5d3934c11f95e6c9a307
tracer-ics2017 <tracer@njuics.org>
2018-04-30 07:50:34 +0000

    >  compile 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  07:50:34 up  1:30,  3 users,  load average: 0.00, 0.00, 0.00 a1c009cf84684ee6abcc36767dcea6b9c8569991

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -207,7 +207,6 @@ opcode_entry opcode_table [512] = {
 
 static make_EHelper(2byte_esc) {
   uint32_t opcode = instr_fetch(eip, 1) | 0x100;
-  printf("******opcode = %x\n",opcode);
 	decoding.opcode = opcode;
   set_width(opcode_table[opcode].width);
   idex(eip, &opcode_table[opcode]);

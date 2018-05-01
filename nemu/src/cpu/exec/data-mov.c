91d33c26314f0f68652862eb140976f1a115d69b
tracer-ics2017 <tracer@njuics.org>
2018-05-01 15:09:58 +0000

    >  compile 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  15:09:58 up 14:16,  6 users,  load average: 0.04, 0.01, 0.00 25e208568c55e9324ea6e7b967bc45cf667072d3

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -65,6 +65,7 @@ make_EHelper(movsx) {
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
 	printf("id_src->width = %d\n",id_src->width);
   rtl_sext(&t2, &id_src->val, id_src->width);
+	printf("t2 = %x\n",t2);
   operand_write(id_dest, &t2);
   print_asm_template2(movsx);
 }

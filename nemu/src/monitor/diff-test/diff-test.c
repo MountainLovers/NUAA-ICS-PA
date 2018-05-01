0d0535e47c501a4096eb05d4a4ddfccf3d5c4229
tracer-ics2017 <tracer@njuics.org>
2018-05-01 05:49:38 +0000

    >  compile 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  05:49:38 up  4:56,  4 users,  load average: 0.01, 0.01, 0.00 db3d9bd4ee3ed6eedec8d6ce9a7a7e1cda3d5d0

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -150,8 +150,15 @@ void difftest_step(uint32_t eip) {
   // TODO: Check the registers state with QEMU.
   // Set `diff` as `true` if they are not the same.
   //TODO();
-  if (cpu.eip != r.eip) diff = true;
-  if (r.eax != cpu.eax || r.ecx != cpu.ecx || r.edx != cpu.edx || r.ebx != cpu.ebx || r.esp != cpu.esp || r.ebp != cpu.ebp || r.esi != cpu.esi || r.edi != cpu.edi) diff = true;
+  if (cpu.eip != r.eip) {diff = true; printf("r.eip = %x <-> cpu.eip = %x\n", r.eip, cpu.eip);}
+  if (cpu.eax != r.eax) {diff = true; printf("r.eax = %x <-> cpu.eax = %x\n", r.eax, cpu.eax);}
+  if (cpu.ebx != r.ebx) {diff = true; printf("r.ebx = %x <-> cpu.ebx = %x\n", r.ebx, cpu.ebx);}
+  if (cpu.ecx != r.ecx) {diff = true; printf("r.ecx = %x <-> cpu.ecx = %x\n", r.ecx, cpu.ecx);}
+  if (cpu.edx != r.edx) {diff = true; printf("r.edx = %x <-> cpu.edx = %x\n", r.edx, cpu.edx);}
+  if (cpu.esp != r.esp) {diff = true; printf("r.esp = %u <-> cpu.esp = %u\n", r.edx, cpu.esp);}
+  if (cpu.ebp != r.ebp) {diff = true; printf("r.ebp = %u <-> cpu.ebp = %u\n", r.esp, cpu.ebp);}
+  if (cpu.esi != r.esi) {diff = true; printf("r.esi = %u <-> cpu.esi = %u\n", r.ebp, cpu.esi);}
+  if (cpu.edi != r.edi) {diff = true; printf("r.edi = %u <-> cpu.edi = %u\n", r.esi, cpu.edi);}
   if (diff) {
     nemu_state = NEMU_END;
   }

8891085da3f497efa8039ba7db5fec36270f9019
tracer-ics2017 <tracer@njuics.org>
2018-05-01 13:21:32 +0000

    >  compile 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  13:21:32 up 12:28,  6 users,  load average: 0.08, 0.02, 0.01 b2e1b631bcdf8674e5a9bdbc884d0e5c6174abbe

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -136,7 +136,8 @@ static inline void rtl_not(rtlreg_t* dest) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  int xx = (1 << 31) >> (32 - width * 8);
+	if ((*src1) >> (width * 8 - 1)) *dest = xx | *src1;
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {

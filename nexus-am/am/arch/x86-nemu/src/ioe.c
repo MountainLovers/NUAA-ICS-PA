a438ad85cc79e8c809e58f1822bd080d59d5a570
tracer-ics2017 <tracer@njuics.org>
2018-05-03 14:28:11 +0000

    >  run 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  14:28:11 up  3:00,  5 users,  load average: 0.09, 0.25, 0.58 5fa21ec27bbc8ae2ec8d80c776cd6f7413ae2683

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -24,7 +24,7 @@ extern void* memcpy(void *, const void *, int);
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
   int i;
   for (i = 0; i < _screen.width * _screen.height; i++) {
-    fb[i] = i;
+    fb[i] = i+i;
    }
 }
 

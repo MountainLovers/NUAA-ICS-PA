d55b3e11cb048a2ec2e588c40783fd8b9a13221d
tracer-ics2017 <tracer@njuics.org>
2018-05-03 11:33:18 +0000

    >  run 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  11:33:18 up 5 min,  3 users,  load average: 0.01, 0.12, 0.08 f18a790d554b2b7d93e1ab287eab7ccbbbe22f4e

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -9,7 +9,8 @@ void _ioe_init() {
 }
 
 unsigned long _uptime() {
-	return inl(RTC_PORT) - boot_time;
+//	return inl(RTC_PORT) - boot_time;
+	return 0;
 }
 
 uint32_t* const fb = (uint32_t *)0x40000;

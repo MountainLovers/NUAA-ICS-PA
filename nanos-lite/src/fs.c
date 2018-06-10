0c241b5063128a03ec34788f14610f9eebe95eb5
tracer-ics2017 <tracer@njuics.org>
2018-06-10 02:00:27 +0000

    >  run 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  02:00:27 up  1:42,  5 users,  load average: 0.00, 0.00, 0.00 86ac7c3a6c926f179ff386d6d86d22b511e1089f

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -4,6 +4,7 @@ typedef struct {
   char *name;
   size_t size;
   off_t disk_offset;
+	off_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
@@ -21,6 +22,47 @@ static Finfo file_table[] __attribute__((used)) = {
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+size_t fs_filesz(int fd) {
+	return file_table[fd].size;
+}
+
+int fs_open(const char *pathname, int flags, int mode) {
+	int i;
+	for (i = 0; i < NR_FILES; i++) {
+		if (strcmp(pathname, file_table[i].name) == 0) {
+			file_table[i].open_offset = 0;
+			return i;
+		}
+	}
+	panic("fs_open failed!");
+	assert(0);
+	return -1;
+}
+
+ssize_t fs_read(int fd, void *buf, size_t len) {
+	if (fd <= 2) {
+		panic("fd < 2 in fs_read");
+		assert(0);
+	}
+	int last_len = file_table[fd].size - file_table[fd].open_offset;
+	if (last_len < len) len = last_len;
+	ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+	file_table[fd].open_offset += len;
+	return len;
+}
+
+/*ssize_t fs_write(int fd, const void *buf, size_t len) {
+}
+
+off_t fs_lseek(int fd, off_t offset, int whence) {
+}*/
+
+int fs_close(int fd) {
+	return 0;
 }

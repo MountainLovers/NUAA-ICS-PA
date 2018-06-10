caf990b3f1d35e89cecf6dfa598da35bc7e57273
tracer-ics2017 <tracer@njuics.org>
2018-06-10 00:35:33 +0000

    >  run 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  00:35:33 up 17 min,  3 users,  load average: 0.08, 0.14, 0.07 e5d2f1183227431897405b0d7f7a3f533d562d34

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -26,6 +26,7 @@ _RegSet* do_syscall(_RegSet *r) {
 						_halt(a[1]);
 						break;
 		case SYS_write:
+						Log("Call SYS_write!!\n");
 						SYSCALL_ARG1(r) = sys_write(a[1], (void *)a[2], a[3]);
 						break;
 		case SYS_brk:

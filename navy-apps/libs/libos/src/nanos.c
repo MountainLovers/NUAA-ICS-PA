a270f08e661bb739598a3d208e93d2e06a0f6d83
tracer-ics2017 <tracer@njuics.org>
2018-06-10 00:50:37 +0000

    >  run 161620112 ZhangZongjie Linux f0d29fb77c8a 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  00:50:37 up 32 min,  5 users,  load average: 0.01, 0.22, 0.25 1daa28b1ff4eb493634579984bd4b689b49de39a

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -34,7 +34,8 @@ int _write(int fd, void *buf, size_t count){
 
 void *_sbrk(intptr_t increment){
 	intptr_t pb_pre = pb;
-	if (_syscall_(SYS_brk, pb_pre + increment, 0, 0) == 0) {
+	int re = _syscall_(SYS_brk, pb_pre + increment, 0, 0);
+	if (re == 0) {
 		pb += increment;
 		return (void *)pb_pre;
 	}else{
